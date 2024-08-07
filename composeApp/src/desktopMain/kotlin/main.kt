ceed2ef061a358d9bb1f01af6f2b1f41eadbdeca
gaurav <gauravraviji@gmail.com>
2024-08-08 03:24:23 +0530

    Windows size set for DesktopApp

--- composeApp/src/desktopMain/kotlin/main.kt
+++ composeApp/src/desktopMain/kotlin/main.kt
@@ -1,22 +1,37 @@
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.window.WindowDraggableArea
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowPosition
+import androidx.compose.ui.window.WindowScope
 import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
-import com.arkivanov.decompose.ComponentContext
 import com.arkivanov.decompose.DefaultComponentContext
 import com.arkivanov.decompose.extensions.compose.lifecycle.LifecycleController
-import com.arkivanov.essenty.lifecycle.Lifecycle
 import com.arkivanov.essenty.lifecycle.LifecycleRegistry
-import org.koin.core.context.GlobalContext
-import org.koin.core.context.GlobalContext.get
-import org.koin.core.module.dsl.singleOf
-import org.koin.dsl.bind
+import org.jetbrains.compose.resources.painterResource
 import org.koin.dsl.module
-import org.koin.mp.KoinPlatform.getKoin
 import ravi.gaurav.learning.tmdb.di.initKoinDesktop
 import ravi.gaurav.learning.tmdb.di.platformComponent.DesktopComponent
 import ravi.gaurav.learning.tmdb.navigation.RootComponent
+import tmdb.composeapp.generated.resources.Res
+import tmdb.composeapp.generated.resources.compose_multiplatform
 
 
 fun main() {
@@ -42,13 +57,15 @@ fun main() {
 
     application {
         val windowState = rememberWindowState()
+        val state = rememberWindowState( size = DpSize(768.dp, 1280.dp)
+        )
 
         LifecycleController(lifecycle, windowState)
 
         Window(
             onCloseRequest = ::exitApplication,
-            state = windowState,
-            title = "TMDB"
+            state = state,
+            title = "The Movie Database",
         ) {
             MaterialTheme {
                 Surface {
@@ -57,4 +74,29 @@ fun main() {
             }
         }
     }
+}
+
+@Composable
+fun WindowScope.AppWindowTitleBar(onMinimize: () -> Unit, onClose: () -> Unit) {
+    AppDraggableArea()
+    Box(Modifier.fillMaxWidth()) {
+        Row(Modifier.align(Alignment.TopEnd).padding(horizontal = 8.dp)) {
+            IconButton(onClick = onMinimize) {
+                // minimize button
+            }
+            IconButton(onClick = onClose) {
+                Icon(painterResource(Res.drawable.compose_multiplatform), null)
+            }
+        }
+    }
+}
+
+@Composable
+private fun WindowScope.AppDraggableArea() = WindowDraggableArea {
+    Box(
+        Modifier.fillMaxWidth().height(18.dp)/*.offset(y = 2.dp)*/
+            .padding(horizontal = 160.dp)
+            .shadow(4.dp, RoundedCornerShape(4.dp, 4.dp, 12.dp, 12.dp), ambientColor = Color.White)
+            .background(Color.Blue)
+    )
 }
