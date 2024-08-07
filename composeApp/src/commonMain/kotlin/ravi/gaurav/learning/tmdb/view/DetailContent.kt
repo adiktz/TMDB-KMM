cbaf73f9da95fd34c2d41dbe4274c2e00e8d56c5
gaurav <gauravraviji@gmail.com>
2024-08-08 04:33:14 +0530

    Detail Screen header

--- composeApp/src/commonMain/kotlin/ravi/gaurav/learning/tmdb/view/DetailContent.kt
+++ composeApp/src/commonMain/kotlin/ravi/gaurav/learning/tmdb/view/DetailContent.kt
@@ -6,26 +6,29 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBackIosNew
+import androidx.compose.material.icons.automirrored.rounded.ArrowBack
+import androidx.compose.material3.FilledTonalIconButton
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
 import io.kamel.image.KamelImage
 import io.kamel.image.asyncPainterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
-import org.koin.compose.koinInject
 import ravi.gaurav.learning.tmdb.navigation.DetailComponent
 import ravi.gaurav.learning.tmdb.util.Constants
-import ravi.gaurav.learning.tmdb.util.UiDesignDecisionHelper
+import ravi.gaurav.learning.tmdb.util.OS
+import ravi.gaurav.learning.tmdb.util.getSystemInsetsHelper
 import ravi.gaurav.learning.tmdb.util.safeHeaderPadding
 
 @Preview
@@ -35,40 +38,57 @@ fun DetailContent(
     modifier: Modifier = Modifier
 ) {
 
-    val uiDesignDecision: UiDesignDecisionHelper = koinInject()
+    val insetsHelper = getSystemInsetsHelper()
     val movie = remember { component.movie }
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .then(
-                if (uiDesignDecision.shouldAddNavigationBarPadding()) {
-                    Modifier.navigationBarsPadding()
-                } else {
-                    Modifier
-                }
-            )
-            .verticalScroll(rememberScrollState())
-    ) {
-        Box {
+    Box {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .then(
+                    if (insetsHelper.os == OS.ANDROID && !insetsHelper.isPortraitMode()) {
+                        Modifier.navigationBarsPadding()
+                    } else {
+                        Modifier
+                    }
+                )
+                .verticalScroll(rememberScrollState())
+        ) {
             KamelImage(
                 resource = asyncPainterResource(Constants.imageBaseUrl + movie.backdropPath),
                 contentDescription = null,
-                modifier = modifier.fillMaxWidth(),
-                contentScale = ContentScale.FillWidth,
+                modifier = modifier.fillMaxWidth()
+                    .height(350.dp),
+                contentScale = ContentScale.Crop,
             )
+        }
 
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .safeHeaderPadding(),
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.Start
-            ) {
-                IconButton(onClick = { component.goBack() }) {
-                    Icon(imageVector = Icons.Default.ArrowBackIosNew, contentDescription = null)
-                }
-                Text("Title: ${movie.title}")
-            }
+        DetailHeader(
+            modifier = Modifier.safeHeaderPadding()
+        ) {
+            component.goBack()
+        }
+
+    }
+}
+
+@Composable
+fun DetailHeader(
+    modifier: Modifier = Modifier,
+    goBack: () -> Unit
+) {
+    Row(
+        modifier = modifier
+            .padding(horizontal = 5.dp)
+            .fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Start
+    ) {
+        FilledTonalIconButton(
+            onClick = { goBack() },
+            modifier = Modifier
+                .alpha(0.7f)
+        ) {
+            Icon(imageVector = Icons.AutoMirrored.Rounded.ArrowBack, contentDescription = null)
         }
     }
 }
